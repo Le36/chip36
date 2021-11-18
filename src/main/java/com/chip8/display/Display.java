ad1765dc1e197beb7785ff88de0f4962a0c33f9d
Le36 <48809753+Le36@users.noreply.github.com>
2021-11-18 13:22:18 +0200

    javafx

---
+++ src/main/java/com/chip8/display/Display.java
@@ -0,0 +1,14 @@
+package com.chip8.display;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Display extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+    }
+
+}

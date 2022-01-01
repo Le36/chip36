bb4a44cbeeeaf8c954554ced5a8573db832b5ae1
Le36 <48809753+Le36@users.noreply.github.com>
2022-01-01 16:43:42 +0200

    include font

--- src/main/java/com/chip8/ui/Ui.java
+++ src/main/java/com/chip8/ui/Ui.java
@@ -3,16 +3,25 @@ package com.chip8.ui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
+import static javafx.scene.text.Font.*;
+
 /**
  * launch javafx application
  */
 public class Ui extends Application {
     /**
      * starts the launcher of emulator
+     * loads the font from resources for the emulator to use
      *
      * @param stage current stage
      */
     public void start(Stage stage) {
+        URL path = getClass().getClassLoader().getResource("fonts/Inconsolata_SemiExpanded-Bold.ttf");
+        loadFont(path.toString(), 12);
+        path = getClass().getClassLoader().getResource("fonts/Inconsolata_SemiExpanded-Regular.ttf");
+        loadFont(path.toString(), 12);
         new Launcher();
     }
 
